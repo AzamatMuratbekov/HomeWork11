b6628d8e561e49840ad964507aa7baa63a21b3a3
Melloman <lovelyboyy@mail.ru>
2022-10-04 09:35:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle uchburchtuk = new Triangle();
+        uchburchtuk.a=7;
+        uchburchtuk.b=9;
+        uchburchtuk.c=4;
+        uchburchtuk.area();
+    }
+}
